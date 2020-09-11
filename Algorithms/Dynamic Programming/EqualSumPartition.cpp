2fdd4987baeae2d71714207c2c02d6a70779ac48
Utsav <43748319+utsavmajhi@users.noreply.github.com>
2020-09-12 05:00:09 +0530

    added

---
+++ Algorithms/Dynamic Programming/EqualSumPartition.cpp
@@ -0,0 +1,60 @@
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+using namespace std; 
+bool subset(int arr[],int sum,int n){
+    bool dp[n+1][sum+1];
+        //initialising
+        for(int i=0;i<n+1;i++){
+            dp[i][0]=true;
+        }
+        for(int i=1;i<sum+1;i++){
+            dp[0][i]=false;
+        }
+        //Case checking
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<sum+1;j++){
+                if(arr[i-1]<=j){
+                    dp[i][j]=dp[i-1][j-arr[i-1]]||dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+}
+bool equalSumpartition(int arr[],int n){
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum=sum+arr[i];
+    }
+    if(sum%2!=0){
+        //not possible
+        return false;
+    }else{
+        //possible
+        return subset(arr,sum/2,n);
+    }
+
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    if(equalSumpartition(arr,n)){
+        cout<<"Possible equal subset found"<<endl;
+    }else{
+        cout<<"Not Possible"<<endl;
+    }
+    
+}
